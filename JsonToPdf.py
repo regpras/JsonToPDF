e151ea5f20a95d82bf532d9cf4958abdf30221bd
regpras <regpras@gmail.com>
2021-03-31 13:36:51 +0530

    Update JsonToPdf.py
    Added Docx file creation.

--- JsonToPdf.py
+++ JsonToPdf.py
@@ -40,6 +40,56 @@ parsedfilepath = r'C:\Prasanna\Python\Data\parsedresult.txt'
 pdfpath = r'C:\Prasanna\Python\Data\TIC.pdf'
 wfp = open(parsedfilepath, 'w', errors='ignore')
 
+# p.add_run('bold').bold = True
+# p.add_run(' and some ')
+# p.add_run('italic.').italic = True
+
+# document.add_heading('Heading, level 1', level=1)
+# document.add_paragraph('Intense quote', style='Intense Quote')
+#
+# document.add_paragraph(
+#     'first item in unordered list', style='List Bullet'
+# )
+# document.add_paragraph(
+#     'first item in ordered list', style='List Number'
+# )
+#
+# document.add_picture('Nifty23May_WD.png', width=Inches(1.25))
+
+# records = (
+#     (3, '101', 'Spam'),
+#     (7, '422', 'Eggs'),
+#     (4, '631', 'Spam, spam, eggs, and spam')
+# )
+#
+# table = document.add_table(rows=1, cols=3)
+# hdr_cells = table.rows[0].cells
+# hdr_cells[0].text = 'Qty'
+# hdr_cells[1].text = 'Id'
+# hdr_cells[2].text = 'Desc'
+# for qty, id, desc in records:
+#     row_cells = table.add_row().cells
+#     row_cells[0].text = str(qty)
+#     row_cells[1].text = id
+#     row_cells[2].text = desc
+
+def createdocx(allmsglist):
+
+    document = Document()
+
+    document.add_heading('Document Title', 0)
+
+    try:
+        for msg in allmsglist:
+            p = document.add_paragraph(msg.text)
+
+            #document.add_page_break()
+        document.add_picture('Nifty23May_WD.png')
+        document.save('demo.docx')
+    except Exception as e:
+        print(e)
+
+
 def createpdf(allmsglist):
     canvas = Canvas(pdfpath)#,  pagesize=letter
     canvas.setFont("Times-Roman", 12)
